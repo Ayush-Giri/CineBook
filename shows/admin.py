19f6768e10811c3cfb8affdc0f2fb1acd5091b5a
Ayush Giri <ayushgiri56@gmail.com>
2026-03-18 15:14:50 +0545

    make modles for shows

--- shows/admin.py
+++ shows/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from shows.models import Shows
 
 # Register your models here.
+
+admin.site.register(Shows)
--- shows/models.py
+++ shows/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from movies.models import Movies
+from theatres.models import Screen
 
 # Create your models here.
+
+class Shows(models.Model):
+    class Meta:
+        verbose_name = "Show"
+        verbose_name_plural = "Shows"
+    movie = models.ForeignKey(Movies, on_delete=models.CASCADE)
+    screen = models.ForeignKey(Screen, on_delete=models.CASCADE)
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    is_active = models.BooleanField(default=True)
+
+
+    def __str__(self):
+        return f"{self.movie.title} | {self.start_time} | {self.end_time} | {self.is_active}"
+
