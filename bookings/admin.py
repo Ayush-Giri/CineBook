e4efa81aaf94601cc1628b6859afa6d05234e5d2
Ayush Giri <ayushgiri56@gmail.com>
2026-03-18 16:22:54 +0545

    make notification models

--- bookings/admin.py
+++ bookings/admin.py
@@ -6,26 +6,3 @@ from bookings.models import Status, BookedSeat, Booking
 admin.site.register(Status)
 admin.site.register(BookedSeat)
 admin.site.register(Booking)
-
-
-# start from payments app
-
-
-
-# reviews app
-# Review
-
-# which user
-# which movie
-# rating (1 to 5)
-# comment
-# created at
-
-# notifications app
-# Notification
-
-# which user
-# title
-# message
-# is read
-# created at
--- notifications/models.py
+++ notifications/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+
+
+User = get_user_model()
+
+
+class Notification(models.Model):
+    class Meta:
+        verbose_name = "Notification"
+        verbose_name_plural = "Notifications"
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=250)
+    message = models.TextField()
+    is_read = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return f"{self.user.username} | {self.title} | \
+            {self.message} | {self.is_read} | {self.created_at}"
